cafc676c955739cb2e5a0b8d1b553fd7740ce5a9
Vadik <ivadik2323@gmail.com>
2015-04-09 21:06:01 +1000

    Add creating panel to filter

--- listview.pas
+++ listview.pas
@@ -6,7 +6,8 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, DBGrids,
-  DBCtrls, StdCtrls, Menus, ExtCtrls, sqldb, DB, MetaData, SQLQueryCreation;
+  DBCtrls, StdCtrls, Menus, ExtCtrls, PairSplitter, sqldb, DB, MetaData,
+  SQLQueryCreation, UMyPanel;
 
 type
 
@@ -14,18 +15,30 @@ type
 
   TListForm = class(TForm)
     AddFilter_btn: TButton;
+    AcceptFilters_btn: TButton;
+    DeleteAllFilters_btn: TButton;
     DataSource: TDataSource;
     DBGrid: TDBGrid;
     DBNavigator1: TDBNavigator;
-    Filter_panel: TPanel;
-    Panel: TPanel;
+    PairSplitter: TPairSplitter;
+    PairSplitterSide1: TPairSplitterSide;
+    PairSplitterSide2: TPairSplitterSide;
+    ScrollBox: TScrollBox;
     SQLQuery: TSQLQuery;
+    procedure AcceptFilters_btnClick(Sender: TObject);
+    procedure AddFilter_btnClick(Sender: TObject);
     procedure DBGridTitleClick(Column: TColumn);
+    procedure DeleteAllFilters_btnClick(Sender: TObject);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormShow(Sender: TObject);
+    procedure RemovePanel(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: integer);
+  private
+    SQLText: string;
   public
     TableTag: integer;
-    FilterArray: array of string;
+    FilterArray: array of TMyParentPanel;
+    FilterArray1: array of string;
     SortArray: array of SortField;
   end;
 
@@ -37,12 +50,14 @@ implementation
 
 {$R *.lfm}
 
+
 { TListForm }
 procedure TListForm.FormShow(Sender: TObject);
 var
   i: integer;
 begin
-  SQLQuery.SQL.Text := SQLQueryCreate(TableTag, SortArray, FilterArray);
+  SQLText := SQLQueryCreate(TableTag, SortArray, FilterArray1);
+  SQLQuery.SQL.Text := SQLText;
   SQLQuery.Open;
   for i := 0 to high(TableArray[TableTag].Fields) do
   begin
@@ -53,6 +68,7 @@ begin
     DBGrid.Columns.Items[i].Visible := TableArray[TableTag].Fields[i].Visible;
   end;
 end;
+
 
 procedure TListForm.FormClose(Sender: TObject; var CloseAction: TCloseAction);
 begin
@@ -78,7 +94,7 @@ begin
     SetLength(SortArray, Length(SortArray) + 1);
     SortArray[High(SortArray)].Name := Column.FieldName;
   end;
-  SQLQuery.SQL.Text := SQLQueryCreate(TableTag, SortArray, FilterArray);
+  SQLQuery.SQL.Text := SQLQueryCreate(TableTag, SortArray, FilterArray1);
   SQLQuery.Close;                                                 //вынести
   SQLQuery.Open;
   for i := 0 to high(TableArray[TableTag].Fields) do
@@ -89,6 +105,83 @@ begin
     DBGrid.Columns.Items[i].Visible := TableArray[TableTag].Fields[i].Visible;
   end;
 end;
+
+procedure TListForm.DeleteAllFilters_btnClick(Sender: TObject);
+begin
+
+end;
+
+procedure TListForm.AddFilter_btnClick(Sender: TObject);
+var
+  i: integer;
+begin
+  SetLength(FilterArray, Length(FilterArray) + 1);
+  if Length(FilterArray) = 1 then
+  begin
+    FilterArray[High(FilterArray)] := TMyFirstPanel.Create(self);
+  end
+  else
+  begin
+    FilterArray[High(FilterArray)] := TMyOtherPanel.Create(self);
+    FilterArray[High(FilterArray)].AndOrBox.Parent := FilterArray[High(FilterArray)];
+  end;
+  with FilterArray[High(FilterArray)] do
+  begin
+    Parent := ScrollBox;
+    Top := 50 + 30 * High(FilterArray);
+    Left := 10;
+    with FieldNames do
+    begin
+      Parent := FilterArray[High(FilterArray)];
+      for i := 0 to (DBGrid.Columns.Count - 1) do
+        Items[i] := DBGrid.Columns.Items[i].Title.Caption;
+    end;
+
+    Conditions.Parent := FilterArray[High(FilterArray)];
+
+    with DeleteButton do
+    begin
+      Parent := FilterArray[High(FilterArray)];
+      OnMouseDown := @RemovePanel;
+      Tag := High(FilterArray);
+    end;
+
+    Edit.Parent := FilterArray[High(FilterArray)];
+  end;
+end;
+
+procedure TListForm.AcceptFilters_btnClick(Sender: TObject);
+begin
+
+end;
+
+procedure TListForm.RemovePanel(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: integer);
+var
+  j: integer;
+begin
+  j := TButton(Sender).Tag;
+  if j <> High(FilterArray) then
+  begin
+    FilterArray[j].Free;
+    for j := j to (High(FilterArray) - 1) do
+    begin
+      FilterArray[j] := FilterArray[j + 1];
+    end;
+    SetLength(FilterArray, Length(FilterArray) - 1);
+    for j := 0 to High(FilterArray) do
+    begin
+      FilterArray[j].DeleteButton.Tag := j;
+      FilterArray[j].Top := 50 + 30 * j;
+    end;
+  end
+  else
+  begin
+    FilterArray[j].Free;
+    SetLength(FilterArray, Length(FilterArray) - 1);
+  end;
+end;
+
 
 initialization
 
